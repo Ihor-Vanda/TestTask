91a2d51ef1c75b7f7e3677cc76bafba5199fe754
Ihor-Vanda <vandaihor@gmail.com>
2025-01-29 22:44:05 +0200

    Added StringProcessor.GetCharCount realisation and test output to Program.cs, fixed path to textfile

--- TestTasks/CharCounting/StringProcessor.cs
+++ TestTasks/CharCounting/StringProcessor.cs
@@ -7,7 +7,31 @@ namespace TestTasks.VowelCounting
     {
         public (char symbol, int count)[] GetCharCount(string veryLongString, char[] countedChars)
         {
-            throw new NotImplementedException();
+            Dictionary<char, int> dictionary = new();
+
+            foreach (char c in countedChars)
+            {
+                dictionary.Add(c, 0);
+            }
+
+            foreach (char c in veryLongString)
+            {
+                if (dictionary.ContainsKey(c))
+                {
+                    dictionary[c]++;
+                }
+            }
+
+            (char symbol, int count)[] result = new (char, int)[dictionary.Count];
+            int index = 0;
+
+            foreach (var x in dictionary)
+            {
+                result[index] = (x.Key, x.Value);
+                index++;
+            }
+
+            return result;
         }
     }
 }
--- TestTasks/Program.cs
+++ TestTasks/Program.cs
@@ -13,15 +13,19 @@ namespace TestTasks
         {
             //Below are examples of usage. However, it is not guaranteed that your implementation will be tested on those examples.            
             var stringProcessor = new StringProcessor();
-            string str = File.ReadAllText("./StringExample.txt");
+            string str = File.ReadAllText("./CharCounting/StringExample.txt");
             var charCount = stringProcessor.GetCharCount(str, new char[] { 'l', 'r', 'm' });
+            // foreach (var (x, y) in charCount)
+            // {
+            //     Console.WriteLine(x + " " + y);
+            // }
 
-            var commodityRepository = new CommodityRepository();
-            commodityRepository.GetImportTariff("Natural honey");
-            commodityRepository.GetExportTariff("Iron/steel scrap not sorted or graded");            
+            // var commodityRepository = new CommodityRepository();
+            // commodityRepository.GetImportTariff("Natural honey");
+            // commodityRepository.GetExportTariff("Iron/steel scrap not sorted or graded");            
 
-            var weatherManager = new WeatherManager();
-            var comparisonResult = await weatherManager.CompareWeather("kyiv,ua", "lviv,ua", 4);
+            // var weatherManager = new WeatherManager();
+            // var comparisonResult = await weatherManager.CompareWeather("kyiv,ua", "lviv,ua", 4);
         }
     }
 }
